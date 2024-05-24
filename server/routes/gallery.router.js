9e301c0056895a94b4a534d596e11eee22f3123c
Chris Harwood <cmharwood79@gmail.com>
2024-05-24 15:24:41 -0500

    updated routes put and get. added images. added components

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const { Pool } = require('pg');
 const router = express.Router();
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+let itemid = req.params.itemid;
+console.log('itemid: ', itemid);
+const queryText = `UPDATE "gallery" SET "likes" = likes + 1 WHERE "id"=$1;`
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlText = `SELECT * FROM "gallery";`;
+  pool 
+  .query(sqlText)
+  .then((result) => {
+    res.send(result.rows);
+  })
+  .catch((error) => {
+    console.log(`Error making database query ${sqlText}`, error);
+  })
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,18 @@
-
 function App() {
-    return (
-      <div>
-        <header>
-          <h1>React Gallery</h1>
-        </header>
+  return (
+    <div>
+      <header>
+        <h1>React Gallery</h1>
+      </header>
 
-        <p>The gallery goes here!</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+      <p>The gallery goes here!</p>
+      <img src='images/goat_small.jpg' />
+      <img src='images/baby_bear.jpg' />
+      <img src='images/big_bear.jpg' />
+      <img src='images/big_horn_sheep.jpg' />
+      <img src='public/images/puzzle.jpg' />
+    </div>
+  );
 }
 
 export default App;
